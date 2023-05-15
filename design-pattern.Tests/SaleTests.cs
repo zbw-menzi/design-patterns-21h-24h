e690725e065436ff62461dd5ba710f0c5d8e0b70
Cédric Menzi <cedric.menzi@buhlergroup.com>
2023-05-15 19:37:50 +0200

    all discounts implemented

--- design-pattern.Tests/SaleTests.cs
+++ design-pattern.Tests/SaleTests.cs
@@ -4,6 +4,8 @@ namespace design_pattern.Tests
 
     using FluentAssertions;
 
+    using Moq;
+
     public class SaleTests
     {
         [Fact]
@@ -15,6 +17,49 @@ namespace design_pattern.Tests
 
             // Act + Assert
             sale.GetTotal().Should().Be(90);
+        }
+
+        [Theory]
+        [InlineData(99, 100, 99)]
+        [InlineData(100, 100, 90)]
+        [InlineData(101, 100, 91)]
+        public void GetTotal_WhenThreshholdMet_Then10Discount(
+            decimal amount,
+            decimal threshold,
+            decimal expected)
+        {
+            // Arrange
+            var strategy = new AbsoluteDiscountPricingStrategy(threshold, 10m);
+            var sale = new Sale(strategy, amount);
+
+            // Act + Assert
+            sale.GetTotal().Should().Be(expected);
+        }
+
+        [Fact]
+        public void GetTotal_WhenBefore12_Then1TimesDiscount()
+        {
+            // Arrange
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            dateTimeProvider.Setup(x => x.Now).Returns(new DateTime(2023, 5, 15, 11, 59, 59));
+            var strategy = new DiscountByTimeOfDayStrategy(10m, dateTimeProvider.Object);
+            var sale = new Sale(strategy, 100m);
+
+            // Act + Assert
+            sale.GetTotal().Should().Be(90);
+        }
+
+        [Fact]
+        public void GetTotal_WhenAfter12_Then1TimesDiscount()
+        {
+            // Arrange
+            var dateTimeProvider = new Mock<IDateTimeProvider>();
+            dateTimeProvider.Setup(x => x.Now).Returns(new DateTime(2023, 5, 15, 12, 01, 01));
+            var strategy = new DiscountByTimeOfDayStrategy(10m, dateTimeProvider.Object);
+            var sale = new Sale(strategy, 100m);
+
+            // Act + Assert
+            sale.GetTotal().Should().Be(80);
         }
     }
 }
