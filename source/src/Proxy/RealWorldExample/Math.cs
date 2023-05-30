ebb8d8ad04c7e629cd72076cfe55cf99a5355523
Ousama Askri <ousama.askri01@gmail.com>
2023-05-30 19:25:09 +0200

    Added Proxy Pattern with Tests. - Used Math Operations as ProxyExample

---
+++ source/src/Proxy/RealWorldExample/Math.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbw.DesignPatterns.Proxy.RealWorldExample
+{
+    // This is the "RealSubject" class
+    internal class Math
+    {
+        public double Add(double x, double y) { return x + y; }
+        public double Sub(double x, double y) { return x - y; }
+        public double Mul(double x, double y) { return x * y; }
+        public double Div(double x, double y) { return x / y; }
+    }
+}
