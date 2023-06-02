fc0ebfa31efa39ca49873db8827c034d8b24b27c
Thomas Solenthaler <tsolenthaler@gmail.com>
2023-06-02 09:04:22 +0200

    add Prototype Pattern

---
+++ source/src/Prototype/BasicCustomer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Zbw.DesignPatterns.Strategy;
+
+namespace Prototype
+{
+    public abstract class BasicCustomer : ICloneable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Address HomeAddress { get; set; }
+        public Address BillingAddress { get; set; }
+
+        public abstract object Clone();
+        public abstract Customer DeepClone();
+    }
+}
---
+++ source/src/Prototype/Customer.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Prototype
+{
+    public class Customer : BasicCustomer
+    {
+        public override object Clone()
+        {
+            return this.MemberwiseClone() as BasicCustomer;
+        }
+        public override Customer DeepClone()
+        {
+            var customer = this.MemberwiseClone() as Customer;
+
+            customer.BillingAddress = new Address
+            {
+                StreetAddress = this.BillingAddress.StreetAddress,
+                City = this.BillingAddress.City,
+                Country = this.BillingAddress.Country,
+                PostCode = this.BillingAddress.PostCode
+            };
+
+            customer.HomeAddress = new Address
+            {
+                StreetAddress = this.HomeAddress.StreetAddress,
+                City = this.HomeAddress.City,
+                Country = this.HomeAddress.Country,
+                PostCode = this.HomeAddress.PostCode
+            };
+
+            return customer;
+        }
+    }
+}
