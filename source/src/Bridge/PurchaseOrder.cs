e44016494b44a0f8f0cd99c9eb76e4048f5138f5
ClauStu <claudio.studerus.gmail.com@zbw-online.ch>
2023-06-05 16:05:20 +0200

    Bridge-Pattern Added

---
+++ source/src/Bridge/PurchaseOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zbw.DesignPatterns.Bridge
+{
+    
+        public class PurchaseOrder : Order
+        {
+            public PurchaseOrder(IPayment payment) : base(payment)
+            {
+            }
+            public override void Checkout(decimal amount)
+            {
+                Payment.SubmitPayment(amount);
+            }
+        }
+}
